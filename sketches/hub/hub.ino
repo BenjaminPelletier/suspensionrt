912ab48f2d9922fd5f17a3b287ce48a9b1a31404
Benjamin Pelletier <pelletier@alumni.caltech.edu>
2024-12-02 15:17:33 -0800

    Use WebServer in hub

--- sketches/hub/hub.ino
+++ sketches/hub/hub.ino
@@ -1,10 +1,31 @@
 #include <WiFi.h>
 #include <NetworkClient.h>
 #include <WiFiAP.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
 
 #include "../wifi_info.h"
 
-NetworkServer server(80);
+WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/plain", "hello from esp32!");
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
 
 void setup() {
   delay(2000);
@@ -21,56 +42,24 @@ void setup() {
   Serial.println(myIP);
   server.begin();
 
-  Serial.println("Server started");
+  if (MDNS.begin("suspensionrt_hub")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
-void loop() {
-  NetworkClient client = server.accept();  // listen for incoming clients
-
-  if (client) {                     // if you get a client,
-    Serial.println("New Client.");  // print a message out the serial port
-    String currentLine = "";        // make a String to hold incoming data from the client
-    while (client.connected()) {    // loop while the client's connected
-      if (client.available()) {     // if there's bytes to read from the client,
-        char c = client.read();     // read a byte, then
-        Serial.write(c);            // print it out the serial monitor
-        if (c == '\n') {            // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
-            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {  // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          Serial.println("Turn LED on");
-        }
-        if (currentLine.endsWith("GET /L")) {
-          Serial.println("Turn LED off");
-        }
-      }
-    }
-    // close the connection:
-    client.stop();
-    Serial.println("Client Disconnected.");
-  }
+void loop(void) {
+  server.handleClient();
+  Serial.println("poll");
+  delay(2);  //allow the cpu to switch to other tasks
 }
