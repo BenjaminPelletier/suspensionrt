967029052bb812e8356bc31228659086ebf95b9f
Benjamin Pelletier <pelletier@alumni.caltech.edu>
2024-12-02 15:32:28 -0800

    Add UDP listener to hub

--- sketches/hub/hub.ino
+++ sketches/hub/hub.ino
@@ -3,10 +3,12 @@
 #include <WiFiAP.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
+#include <AsyncUDP.h>
 
 #include "../wifi_info.h"
 
 WebServer server(80);
+AsyncUDP udp;
 
 void handleRoot() {
   server.send(200, "text/plain", "hello from esp32!");
@@ -25,6 +27,24 @@ void handleNotFound() {
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
+}
+
+void onUDPPacket(AsyncUDPPacket packet) {
+  /*Serial.print("UDP Packet Type: ");
+  Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
+  Serial.print(", From: ");
+  Serial.print(packet.remoteIP());
+  Serial.print(":");
+  Serial.print(packet.remotePort());
+  Serial.print(", To: ");
+  Serial.print(packet.localIP());
+  Serial.print(":");
+  Serial.print(packet.localPort());
+  Serial.print(", Length: ");
+  Serial.println(packet.length());*/
+  Serial.print(packet.remoteIP());
+  Serial.print(": ");
+  Serial.println((char*)packet.data());
 }
 
 void setup() {
@@ -56,10 +76,21 @@ void setup() {
 
   server.begin();
   Serial.println("HTTP server started");
+
+  if(!udp.listen(udp_port)) {
+    log_e("UDP listening failed.");
+    while (1);
+  }
+  udp.onPacket(onUDPPacket);
+  Serial.print("UDP Listening on ");
+  Serial.print(WiFi.localIP());
+  Serial.print(":");
+  Serial.println(udp_port);
+
+  WiFi.setSleep(false);
 }
 
 void loop(void) {
   server.handleClient();
-  Serial.println("poll");
   delay(2);  //allow the cpu to switch to other tasks
 }
